9666a5625877b797cabcf13e4aa0dbf3cfad684f
wakiko888 <joachim.galilee@gmail.com>
2025-04-06 19:24:28 +0200

    Intitialising

---
+++ Calc.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace C#
+{
+    public class Calc
+    {
+        /// <summary>
+        /// Add 2 int (Number 1 and Number 2) into another int.
+        /// </summary>
+        /// <param name="nb1">Number 1</param>
+        /// <param name="nb2">Number 2</param>
+        /// <returns>Number 1 + Number 2</returns>
+        public int addition(int nb1 int nb2)
+        {
+            return nb1 + nb2
+        }
+
+
+        /// <summary>
+        /// Substract 2 int (Number 1 and Number 2) into another int.
+        /// </summary>
+        /// <param name="nb1">Number 1</param>
+        /// <param name="nb2">Number 2</param>
+        /// <returns>Number 1 - Number 2</returns>
+        public int Substraction(int nb1 int nb2)
+        {
+            return nb1 - nb2
+        }
+
+        /// <summary>
+        /// multiply 2 int (Number 1 and Number 2) into another int.
+        /// </summary>
+        /// <param name="nb1">Number 1</param>
+        /// <param name="nb2">Number 2</param>
+        /// <returns>Number 1 X Number 2</returns>
+        public int multiplication(int nb1 int nb2)
+        {
+            return nb1 * nb2
+        }
+
+        /// <summary>
+        /// Divide 2 int (Number 1 and Number 2) into another int.
+        /// </summary>
+        /// <param name="nb1">Number 1</param>
+        /// <param name="nb2">Number 2</param>
+        /// <returns>Number 1 / Number 2</returns>
+        public int Division(int nb1 int nb2)
+        {
+            return nb1 / nb2
+        }
+
+        /// <summary>
+        /// Divide and take the rest of 2 int (Number 1 and Number 2) into another int.
+        /// </summary>
+        /// <param name="nb1">Number 1</param>
+        /// <param name="nb2">Number 2</param>
+        /// <returns>Number 1 % Number 2</returns>
+        public int addition(int nb1 int nb2)
+        {
+            return nb1 % nb2
+        }
+
+    }
+}
